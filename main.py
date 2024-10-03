34cab6c0647d47220ad194dbab9038c8db5446b8
Anyelo1002 <141687585+Anyelo1002@users.noreply.github.com>
2024-10-02 22:03:20 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+import clase
+
+# Crear un objeto de RandomCalculator
+random_calculator = clase.RandomCalculator()
+
+# Realizar un cálculo aleatorio
+result = random_calculator.calculate()
+print(result)
