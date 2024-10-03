792d8df511dc125f449beb964441ceeb3d9e057b
Anyelo1002 <141687585+Anyelo1002@users.noreply.github.com>
2024-10-02 22:09:51 -0500

    Create Clase.py

---
+++ Clase.py
@@ -0,0 +1,24 @@
+import random
+
+class BasicOperations:
+   def add(self, a, b):
+      return a + b
+       
+   def sub(self, a, b):
+      return a - b
+   
+   def mul(self, a, b):
+      return a * b
+   
+   def div(self, a, b):
+      if b != 0:
+         return a / b
+      else:
+         return "No se puede dividir por cero"
+
+
+        #eleva el numero por un valor
+class Square:
+   def getVal(self, val):
+      return val * val
+
